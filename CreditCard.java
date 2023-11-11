07670df514202149e226bcf4af16a14295a5e05f
sreenu <sreenu.sreenutech@gmail.com>
2023-11-11 11:50:59 +0530

    added m2() file

--- CreditCard.java
+++ CreditCard.java
@@ -12,6 +12,11 @@ System.out.println("Credit Card Application ");
 	public void m1(){
  
              System.out.println("m1() method");
+	}
+
+	public void m2(){
+ 
+             System.out.println("m2() method");
 	}	
 	
 	
