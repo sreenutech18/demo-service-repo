04e447965787565b62e210737765c7deed36f0b2
sreenu <sreenu.sreenutech@gmail.com>
2024-03-23 15:00:55 +0530

    updated credit card file

--- CreditCard.java
+++ CreditCard.java
@@ -6,6 +6,13 @@ public static void main(String[] args){
 	 boolean flag = false;
      System.out.println("Credit Card Application ");
 
+}
+
+public  void m1(){
+
+	
+     System.out.println("This is m1() method ");
+
 }	
 	
 	
