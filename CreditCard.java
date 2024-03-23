fde21fbeb734de38d78c0f0eb34893e3c497c452
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2024-03-23 15:18:37 +0530

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -2,12 +2,9 @@ public class CreditCard{
 	
 
 public static void main(String[] args){
-
-	 
      System.out.println("Credit Card Application ");
 
 }
-
 public  void m1(){
    System.out.println("This is m1() method ");
 }
@@ -19,6 +16,9 @@ public  void m2(){
 public  void m3(){
    System.out.println("This is m2() method ");
 }
-	
+
+public  void m4(){
+   System.out.println("This is m4() method ");
+}
 	
 }	
