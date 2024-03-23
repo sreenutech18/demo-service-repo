be83f321b0338df16e0e79ac58a3e91d33db72c3
sreenu <sreenu.sreenutech@gmail.com>
2024-03-23 14:57:21 +0530

    credit card file changes

--- CreditCard.java
+++ CreditCard.java
@@ -4,7 +4,7 @@ public class CreditCard{
 public static void main(String[] args){
 
 	 boolean flag = false;
-System.out.println("### ***********Credit Card Application **********### ");
+     System.out.println("Credit Card Application ");
 
 }	
 	
